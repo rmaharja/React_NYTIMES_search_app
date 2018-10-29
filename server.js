c8441c2eefbb403aa793c42d95390da8c3951d12
rmaharja@gmail.com <rmaharja@gmail.com>
2018-10-29 19:53:08 -0400

    added mongoose

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 
 app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 const PORT = process.env.PORT || 3001;
 
+//Connecting to MongoDb 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reacnytimesdb",
+  {
+    useMongoClient: true
+  }
+);
 
 
 // Start the API server
