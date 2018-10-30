f9b4cc4b93dd3b59cbb540bf69c871e06a516925
rmaharja@gmail.com <rmaharja@gmail.com>
2018-10-29 20:31:31 -0400

    adding react for Heroku deploy

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+//Routing
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 3001;
 
 //Connecting to MongoDb 
