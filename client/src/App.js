587f6d710e8613fc8ec84f9a37d4028dd1852d27
rmaharja@gmail.com <rmaharja@gmail.com>
2018-10-29 22:08:22 -0400

    added all other routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import Jumbotron from "./components/Jumbotron";
 import Container from "./components/Container";
 import Search from "./components/Search";
- 
 
 class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Jumbotron></Jumbotron>
-          <Router>
-          <Switch>
-            <Route exact path ="/" component={Search}></Route>
-          </Switch>
-          </Router>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Jumbotron></Jumbotron>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Search}></Route>
+                        <Route component={Search}></Route>
+                    </Switch>
+                </Router>
+            </Container>
+        );
+    }
 }
 
 export default App;
